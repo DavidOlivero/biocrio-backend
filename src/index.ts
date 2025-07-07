d1a0581d968a26f8e28d9f5ad6c3cc2ee67bcd47
DavidOlivero <daniolidavid32@gmail.com>
2025-07-07 09:50:13 -0500

    Add initial server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
-//  NOTE: Todo lo mencionado acá es cierto. NO mentira solo es un mensaje de prueba, mi hermano no es un alienígena tiene la forma de una maza rara
-console.log('Hola mundo, ayuda por favor tengo un hermano como desqueciado estoy buncando la manera de escapar pero no puedo ayudaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa!')
-console.log('Bitakora del capitán, actualizo mi estado, estado de gravedadad, mi hermano ha cambiado de forma ha una especie de maza blanda en estado de ingrabidez')
+import express from "express";
+
+const app = express()
+const PORT = 3000
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to biocrio api' })
+})
+app.listen(PORT, () => console.log(`Server statarted at port http://localhost:${PORT}`))
