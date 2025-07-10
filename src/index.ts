6b7c1fc2bed122777152298a59d6f86ae2d0d9d2
DavidOlivero <daniolidavid32@gmail.com>
2025-07-10 14:09:35 -0500

    Make the hexagonal achitecture

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
-import express from "express";
+import app from "config/server"
 
-const app = express()
 const PORT = 3000
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to biocrio api' })
-})
 app.listen(PORT, () => console.log(`Server statarted at port http://localhost:${PORT}`))
